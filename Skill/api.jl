aeec43e43b9ba8885829dab933ee864c3ac8c128
andreasdominik <andreas.dominik@mni.thm.de>
2019-06-11 14:00:47 +0200

    tv-Viera API impl.

--- Skill/api.jl
+++ Skill/api.jl
@@ -2,3 +2,30 @@
 # API function goes here, to be called by the
 # skill-actions:
 #
+
+function tvON()
+
+    trigger = Dict(:room => Snips.getSiteId(),
+                   :device => Snips.getConfig(INI_TV),
+                   :commands => ["susi", "wait20",
+                                 "TV", "wait1", 
+                                 "AV", "up", "up", "up",
+                                 "center"],
+                   :delay => 0.5
+                   )
+
+    Snips.publishSystemTrigger("ADoSnipsTVViera", trigger)
+end
+
+
+
+function tvOFF()
+
+    trigger = Dict(:room => Snips.getSiteId(),
+                   :device => Snips.getConfig(INI_TV),
+                   :commands => ["TV", "standby"],
+                   :delay => 0.5
+                   )
+
+    Snips.publishSystemTrigger("ADoSnipsTVViera", trigger)
+end
