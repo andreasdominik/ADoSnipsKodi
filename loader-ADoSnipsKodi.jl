03ef9ce05871798adb20e84f5063095e31af6be5
andreasdominik <andreas.dominik@mni.thm.de>
2019-06-11 13:09:12 +0200

    Inital commit for new Skill ADoSnipsKodi

--- loader-ADoSnipsKodi.jl
+++ loader-ADoSnipsKodi.jl
@@ -8,8 +8,8 @@
 #
 
 APP_DIR = @__DIR__
-include("$APP_DIR/Skill/ADoSnipsTemplate.jl")
-import Main.ADoSnipsTemplate
+include("$APP_DIR/Skill/ADoSnipsKodi.jl")
+import Main.ADoSnipsKodi
 
 global INTENT_ACTIONS
-append!(INTENT_ACTIONS, ADoSnipsTemplate.getIntentActions())
+append!(INTENT_ACTIONS, ADoSnipsKodi.getIntentActions())
