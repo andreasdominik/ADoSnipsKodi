6ec577c814bed87cd188f1d82fce5f0e20992c80
andreas.dominik <andreas.dominik@mni.thm.de>
2019-06-21 11:11:29 +0200

    translated to en

--- Skill/languages.jl
+++ Skill/languages.jl
@@ -28,8 +28,36 @@ Snips.addText("de", :which_pictures, """Ich habe nicht verstanden,
         welche Fotos ich zeigen soll. Ich öffne die Datenbank!""")
 Snips.addText("en", :be_precise, "Bitte präzisiere Deine Frage!")
 
-Snips.addText("en", :noip, "I am not able to find the IP address of kodi in config.ini")
-Snips.addText("en", :dunno, "I don't know, what to do")
-Snips.addText("en", :switchon, "I switch in the tv and kodi. Please be patient!")
-Snips.addText("en", :switchoff, "I switch off the tv and kodi.")
+
+
+
+Snips.addText("en", :switchon, "I have to power on the TV. Please be patient; this will takle some time.")
+Snips.addText("en", :switchoff, "T power off the TV and Kodi.")"
 Snips.addText("en", :ok, "OK.")
+Snips.addText("en", :diy, "I open Kodi; please select in the menu what you want to see.")
+Snips.addText("en", :i_play, "I start playing")
+Snips.addText("en", :i_play_new_otr, "I play the next recording")
+Snips.addText("en", :season, "season")
+Snips.addText("en", :episode, "episode")
+Snips.addText("en", :episodes, "episodes")
+Snips.addText("en", :recordings, "recordings")
+Snips.addText("en", :slideshows, "slide shows")
+Snips.addText("en", :title_is, "the title is")
+Snips.addText("en", :i_search_show, "I search the TV show database for")
+Snips.addText("en", :i_search_movie, "I search the movie database for")
+Snips.addText("en", :i_search_rec, "I serach the recordings for")
+Snips.addText("en", :be_patient, "Please be patient; this might take a moment!")
+Snips.addText("en", :found, "found")
+Snips.addText("en", :fits, "fit to your request")
+Snips.addText("en", :new, "unseen")
+
+Snips.addText("en", :noip, "I cannot find the address of Kodi in the config.ini!")
+Snips.addText("en", :dunno, "I don't know what to do!")
+Snips.addText("en", :error_kodiCmd, "I cannot execute the Kodi command.")
+Snips.addText("en", :error_name, "Unfortunately, I could not understand which movie I should show!")"
+Snips.addText("en", :error_on, "I can not turn Kodi on!")
+
+Snips.addText("en", :which_pictures, """I did not understand,
+        which photos I should show. I open the database!""")
+
+Snips.addText("en", :be_precise, "Please specify your question!")
